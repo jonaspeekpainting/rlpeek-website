3961bba1765f9e14dc279e9f6d0b54c65d6f8731
jonaspeekpainting <jonas@rlpeekpainting.com>
2026-03-07 10:33:09 -0800

    One more seo fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  poweredByHeader: false,
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
   images: {
     remotePatterns: [
